3cb48e38a17981cc02a4cd677c763615fdf79e4c
ghalebshhab <ghalebshhab12@icloud.com>
2026-09-05 16:51:42 +0300

    feat(employment): add employment DTOs for the leave request

---
+++ src/main/java/com/salon/backend/DTOs/Salon/Employment/Leave/CreateLeaveRequest.java
@@ -0,0 +1,13 @@
+package com.salon.backend.DTOs.Salon.Employment.Leave;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLeaveRequest {
+    String reason;
+
+}
